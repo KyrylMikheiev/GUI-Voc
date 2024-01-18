0b48bb007983cae1947478a27613275c22d036a4
KyrylMikheiev <kyrylmikheiev20.11@gmail.com>
2024-01-18 23:14:26 +0100

    deleted unused imports

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,18 @@ package src;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
-
-import javafx.scene.text.Font;
 
 public class Main { 
     public Main() {
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,16 +1,6 @@
 package src;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Image;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 
 public class MainMenu {
 
@@ -25,7 +15,6 @@ public class MainMenu {
         contentPane = new JPanel();
         //create elements and add them to the contentPane
 
-        JButton menuButton = new JButton();
 
 
         body.add(contentPane);
